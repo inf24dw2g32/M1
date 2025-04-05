9fe9f7488149244f08e8ecc0d94f4711ff4c553c
João - A045235 <160974569+joaoismai@users.noreply.github.com>
2025-04-05 19:45:41 +0100

    Add files via upload

---
+++ work/routes/auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+// Rota para redirecionar para o Google OAuth
+router.get('/google', passport.authenticate('google', {
+    scope: ['profile', 'email'] // Define os escopos de acesso que você deseja
+}));
+
+// Rota de callback após a autenticação do Google
+router.get('/google/callback', passport.authenticate('google', { session: false }), (req, res) => {
+    // Se a autenticação for bem-sucedida, 'req.user' terá as informações do usuário
+    res.json({
+        message: 'Autenticação bem-sucedida',
+        user: req.user, // Aqui você pode pegar as informações do usuário
+    });
+});
+
+module.exports = router;
